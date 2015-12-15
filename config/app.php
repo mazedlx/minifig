35e3c60f0f60d1f99ac15cb482c327d990014f0c
Christian Leo-Pernold <mazedlx@gmail.com>
2015-12-15 08:27:22 +0100

    install

--- config/app.php
+++ config/app.php
@@ -26,7 +26,7 @@ return [
     |
     */
 
-    'url' => 'http://localhost',
+    'url' => 'http://alderaan.mazedlx.net/minifig',
 
     /*
     |--------------------------------------------------------------------------
