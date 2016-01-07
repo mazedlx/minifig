364743eaf1b6b91e410caac6e52f731118d01f05
Christian Leo <mazedlx@gmail.com>
2016-01-07 16:55:04 +0100

    Code Cleanup

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -34,10 +34,11 @@
 		</nav>
 		<div class="container">
 			<div class="row">
-			@if (Session::has('msg'))
-    		<div class="alert alert-success alert-dismissible" role="alert">{{ Session::get('msg') }} <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>
-			@endif
-			@yield('content')
+				@if (Session::has('msg'))
+	    			<div class="alert alert-success alert-dismissible" role="alert">{{ Session::get('msg') }} <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>
+				@endif
+
+				@yield('content')
 			</div>
 		</div>
 		<script>
