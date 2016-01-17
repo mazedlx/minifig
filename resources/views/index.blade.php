d0adcc94f4f11e2e0d7e17d37b8b06c714a66969
Christian Leo <mazedlx@gmail.com>
2016-01-17 12:13:49 +0100

    Latest minifig and set now link to their details page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,11 +6,11 @@
 <div class="row">
     <div class="col-md-6">
         <h4>Latest Minifig: {{ $latest_minifig->name }}</h4>
-        <img src="{{ url()->to('uploads/' . $latest_minifig->images->first()->filename) }}" class="img img-thumbnail" width="300px">
+        <a href="{{ url()->to('minifigs/' . $latest_minifig->id) }}"><img src="{{ url()->to('uploads/' . $latest_minifig->images->first()->filename) }}" class="img img-thumbnail" width="300px"></a>
     </div>
     <div class="col-md-6">
         <h4>Latest Model: {{ $latest_set->name }} ({{ $latest_set->number }})</h4>
-        <img src="{{ url()->to('uploads/' . $latest_set->filename) }}" class="img img-thumbnail" width="300px">
+        <a href="{{ url()->to('sets/' . $latest_set->id) }}"><img src="{{ url()->to('uploads/' . $latest_set->filename) }}" class="img img-thumbnail" width="300px"></a>
     </div>
 </div>
  @stop
